ef317889b0dc6d2eb1decdf2595069c734162ec4
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-06-07 17:50:53 +0100

    add basic api key validation comparison to keys in database

--- src/middleware/apiKeys.ts
+++ src/middleware/apiKeys.ts
@@ -1,40 +1,37 @@
 import express, { NextFunction, Request, Response } from "express";
+import { db } from "../lib/db"
 
+
+// function to generate a random 36 character string for the api key // 
+// This would be called upon a new user being generated when they sign up for a key 
+// Key would then be inserted into database. Would be hashed at some point along the journey for added security. 
 const genKey = () => {
     return [...Array(30)]
         .map((e) => Math.floor(Math.random() * 36).toString(36))
         .join('')
 }
 
-console.log(genKey())
+// Function to validate API key //
+export async function validateApiKey(req: Request, res: Response, next: NextFunction) { // exported to be used in router.ts
 
-export function validateApiKey(req: Request, res: Response, next: NextFunction) {
+    const apiKey = req.headers['x-api-key']; // define apiKey from headers of the request
 
-    const apiKey = req.headers['x-api-key'];
-    if (apiKey === 'xc55knmumgp7qgw4k0miqnov6y105x') {
-        console.log('Api Key Match! You can proceed')
-        next()
-    } else {
+    if (!apiKey) {
         res.status(401).json({ error: 'Unauthorized. Api Key is missing or incorrect.' })
+    } else if (apiKey) {
+        try {
+            const findKey = await db.query('SELECT * from users WHERE key = $1', [apiKey])
+            if (findKey.rows.length === 0) {
+                res.status(401).json({ error: 'Unauthorized. Api key is missing or incorrect.' })
+                console.error("Api Key does not exist in datadase.")
+            } else {
+                console.log('Api Key Match!')
+                next() // if key matches, proceed to endpoint called.
+            }
+        } catch (error) {
+            console.error("Database query error: ", error)
+        }
     }
-
-
-
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// module.exports(validateApiKey)
