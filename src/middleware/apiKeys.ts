6c10bf9f15dcb005520ca83707630979d7eb731a
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-06-12 23:44:47 +0100

    add api useage log aggregation with cron job

--- src/middleware/apiKeys.ts
+++ src/middleware/apiKeys.ts
@@ -3,6 +3,7 @@ import { db } from "../lib/db"
 import { logger } from "../middleware/logger"
 
 
+
 // function to generate a random 36 character string for the api key // 
 // This would be called upon a new user being generated when they sign up for a key 
 // Key would then be inserted into database. Would be hashed at some point along the journey for added security. 
@@ -18,20 +19,22 @@ export async function validateApiKey(req: Request, res: Response, next: NextFunc
     const apiKey = req.headers['x-api-key']; // define apiKey from headers of the request
 
     if (!apiKey) {
-        res.status(401).json({ error: 'Unauthorized. Api Key is missing or incorrect.' })
+        res.status(401).json({ error: 'Unauthorized. Api Key is missing.' })
         logger(req, res)
+
     } else if (apiKey) {
         try {
-            const findKey = await db.query('SELECT * from users WHERE api_key = $1', [apiKey])
-            if (findKey.rows.length === 0) {
-                res.status(401).json({ error: 'Unauthorized. Api key is missing or incorrect.' })
+            const findKey = await db.query('SELECT EXISTS(SELECT 1 FROM users WHERE api_key = $1)', [apiKey])
+            // console.log("API key match =", findKey.rows[0].exists)
+            if (findKey.rows[0].exists === 'false') {
+                res.status(401).json({ error: 'Unauthorized. Api key is incorrect.' })
                 logger(req, res)
-                console.error("Api Key does not exist in datadase.")
             } else {
                 console.log('Api Key Match!')
+                logger(req, res)
                 next() // if key matches, proceed to endpoint called.
             }
-        } catch (error) {
+        } catch (error: any) {
             console.error("Database query error: ", error)
         }
     }
