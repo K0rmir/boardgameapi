0e6fb350bd767b7adca095d69f319beceee64c7a
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-06-14 22:41:06 +0100

    update

---
+++ client/lib/db.ts
@@ -0,0 +1,20 @@
+// Connect to Supabase database //
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const dbConnectionString = process.env.DATABASE_URL;
+
+if (!dbConnectionString) {
+    throw new Error("DATABASE_URL is not set in the environment variables"); // Throw error if no connection string is provided for database.
+}
+
+export const db = new pg.Pool({ connectionString: dbConnectionString });
+
+db.on('error', (err) => { // add event listener 'on' to handle any unexpected errors that occur while client is idle.
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+});
+
+console.log("Database pool created with connection string");
