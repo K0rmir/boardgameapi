a2e4c55ceb52965ab8d6342236253f259173aead
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-07-09 21:19:53 +0100

    update page limit per request

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,6 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
 import { db } from "../lib/db"
-import cron from 'node-cron';
+// import cron from 'node-cron';
 
 export async function logger(req: Request, res: Response, apiKey?: string) {
 
