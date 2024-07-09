537564cef05e3856eff240f42ece03fc2879ae7d
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-07-09 17:42:48 +0100

    update index.ts for vercel deployment

--- src/index.ts
+++ src/index.ts
@@ -43,15 +43,15 @@ const startServer = async () => {
         });
 
         // Start the server
-        // app.listen(PORT, () => {
-        //     console.log(`Server is running on http://localhost:${PORT}`);
-        // });
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
     } catch (err) {
         console.error('Failed to connect to the database', err);
         process.exit(1);
     }
 };
 
-startServer();
+// startServer();
 
 export default app;
