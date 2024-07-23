a6523dcfac2a79523478ab660d8ace1b108431fa
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-07-23 22:27:44 +0100

    update vercel cronjob

--- src/api/[cron].ts
+++ src/api/[cron].ts
@@ -1,6 +1,6 @@
-import { db } from "../../lib/db"
+import { db } from "../lib/db"
 
-export default async function aggregateLogs() {
+export default async function aggregateLogs(req, res) {
     console.log("--CronJob Start--");
 
     const date = new Date();
@@ -12,10 +12,10 @@ export default async function aggregateLogs() {
     if (checkLogsToday.rows.length > 0) {
         await db.query(`INSERT INTO api_usage_aggregate (api_key, endpoint, date, request_count, avg_response_time_ms, error_count) SELECT api_key, endpoint, DATE_TRUNC('day', timestamp) AS date, COUNT(*) AS request_count, AVG(response_time_ms) AS avg_response_time_ms, COUNT(*) FILTER(WHERE status_code >= 400) AS error_count FROM api_usage_logs WHERE timestamp >= NOW() - INTERVAL '1 day' GROUP BY api_key, endpoint, DATE_TRUNC('day', timestamp)`);
         console.log(`Logs found on ${today} & aggregated.`);
-        console.log("--CronJob End--");
     } else {
         await db.query('INSERT INTO api_usage_aggregate (api_key, endpoint, date, request_count, avg_response_time_ms, error_count) VALUES ($1, $2, $3, $4, $5, $6)', ['NULL', '/', today, 0, 0, 0]);
         console.log(` No logs found on ${today}. Inserted zeros.`);
-        console.log("--CronJob End--");
     }
+
+    res.status(200).json({ message: "Cron job completed" });
 }
