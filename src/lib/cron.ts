48d626e2851dc5c3ab5c125c09184e7a6c62792f
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-07-23 21:58:37 +0100

    update cron to aggregate zero usage days accordingly

--- src/lib/cron.ts
+++ src/lib/cron.ts
@@ -1,7 +1,21 @@
 import { db } from "../lib/db"
 
 export default async function aggregateLogs() {
-    console.log("--CronJob Start--")
-    await db.query(`INSERT INTO api_usage_aggregate (api_key, endpoint, date, request_count, avg_response_time_ms, error_count) SELECT api_key, endpoint, DATE_TRUNC('day', timestamp) AS date, COUNT(*) AS request_count, AVG(response_time_ms) AS avg_response_time_ms, COUNT(*) FILTER(WHERE status_code >= 400) AS error_count FROM api_usage_logs WHERE timestamp >= NOW() - INTERVAL '1 day' GROUP BY api_key, endpoint, DATE_TRUNC('day', timestamp)`)
-    console.log("--CronJob End--")
+    console.log("--CronJob Start--");
+
+    const date = new Date();
+    const today = date.toLocaleDateString('en-CA');
+
+    // Check if there are any logs today, if so aggregate, if not, insert zero's //
+    const checkLogsToday: any = await db.query(`SELECT timestamp FROM api_usage_logs WHERE DATE(timestamp) = $1`, [today]);
+
+    if (checkLogsToday.rows.length > 0) {
+        await db.query(`INSERT INTO api_usage_aggregate (api_key, endpoint, date, request_count, avg_response_time_ms, error_count) SELECT api_key, endpoint, DATE_TRUNC('day', timestamp) AS date, COUNT(*) AS request_count, AVG(response_time_ms) AS avg_response_time_ms, COUNT(*) FILTER(WHERE status_code >= 400) AS error_count FROM api_usage_logs WHERE timestamp >= NOW() - INTERVAL '1 day' GROUP BY api_key, endpoint, DATE_TRUNC('day', timestamp)`);
+        console.log(`Logs found on ${today} & aggregated.`);
+        console.log("--CronJob End--");
+    } else {
+        await db.query('INSERT INTO api_usage_aggregate (api_key, endpoint, date, request_count, avg_response_time_ms, error_count) VALUES ($1, $2, $3, $4, $5, $6)', ['NULL', '/', today, 0, 0, 0]);
+        console.log(` No logs found on ${today}. Inserted zeros.`);
+        console.log("--CronJob End--");
+    }
 }
--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,7 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import { db } from "../lib/db"
 // import cron from 'node-cron';
-import { CronJob } from 'cron';
+// import { CronJob } from 'cron';
 
 export async function logger(req: Request, res: Response, apiKey?: string) {
 
@@ -18,18 +18,34 @@ export async function logger(req: Request, res: Response, apiKey?: string) {
     }
 }
 
+// Cron job functions for local testing //
+
 // const job = new CronJob(
 //     '*/1 * * * *',
 //     async function () {
-//         console.log("--CronJob Start--")
-//         const aggregateLogs = await db.query(`INSERT INTO api_usage_aggregate (api_key, endpoint, date, request_count, avg_response_time_ms, error_count) SELECT api_key, endpoint, DATE_TRUNC('day', timestamp) AS date, COUNT(*) AS request_count, AVG(response_time_ms) AS avg_response_time_ms, COUNT(*) FILTER(WHERE status_code >= 400) AS error_count FROM api_usage_logs WHERE timestamp >= NOW() - INTERVAL '1 day' GROUP BY api_key, endpoint, DATE_TRUNC('day', timestamp)`)
-//         console.log("--CronJob End--")
+//         console.log("--CronJob Start--");
+
+//         const date = new Date();
+//         const today = date.toLocaleDateString('en-CA');
+
+//         // Check if there are any logs today, if so aggregate, if not, insert zero's //
+//         const checkLogsToday: any = await db.query(`SELECT timestamp FROM api_usage_logs WHERE DATE(timestamp) = $1`, [today]);
+
+//         if (checkLogsToday.rows.length > 0) {
+//             const aggregateLogs = await db.query(`INSERT INTO api_usage_aggregate (api_key, endpoint, date, request_count, avg_response_time_ms, error_count) SELECT api_key, endpoint, DATE_TRUNC('day', timestamp) AS date, COUNT(*) AS request_count, AVG(response_time_ms) AS avg_response_time_ms, COUNT(*) FILTER(WHERE status_code >= 400) AS error_count FROM api_usage_logs WHERE timestamp >= NOW() - INTERVAL '1 day' GROUP BY api_key, endpoint, DATE_TRUNC('day', timestamp)`);
+//             console.log("Logs found & aggregated");
+//             console.log("--CronJob End--");
+//         } else {
+//             await db.query('INSERT INTO api_usage_aggregate (api_key, endpoint, date, request_count, avg_response_time_ms, error_count) VALUES ($1, $2, $3, $4, $5, $6)', ['NULL', '/', today, 0, 0, 0]);
+//             console.log(` No logs found on ${today}. Inserted zeros`);
+//             console.log("--CronJob End--");
+//         }
 //     },
 //     null,
 //     true
-// )
+// );
 
-// const aggregateLogs = cron.schedule('*/1 * * * *', async () => {
+// const aggregateLogs = cron.schedule('0 1 * * *', async () => {
 //     console.log("--CronJob Start--")
 //     await db.query(`INSERT INTO api_usage_aggregate (api_key, endpoint, date, request_count, avg_response_time_ms, error_count) SELECT api_key, endpoint, DATE_TRUNC('day', timestamp) AS date, COUNT(*) AS request_count, AVG(response_time_ms) AS avg_response_time_ms, COUNT(*) FILTER(WHERE status_code >= 400) AS error_count FROM api_usage_logs WHERE timestamp >= NOW() - INTERVAL '1 day' GROUP BY api_key, endpoint, DATE_TRUNC('day', timestamp)`)
 //     console.log("--CronJob End--")
