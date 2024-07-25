af32b30bf745794f3b41b147d046736a5e5750cf
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-07-25 17:13:03 +0100

    update cron.ts

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/boardgames", boardGamesRouter); // (*)
 
-// Cron //
+// Cron route/endpoint //
 app.get('/utils/cron', async (req, res) => {
     try {
         await aggregateLogs();
--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,6 +1,5 @@
 import express, { NextFunction, Request, Response } from "express";
 import { db } from "../lib/db"
-// import cron from 'node-cron';
 // import { CronJob } from 'cron';
 
 export async function logger(req: Request, res: Response, apiKey?: string) {
@@ -11,10 +10,10 @@ export async function logger(req: Request, res: Response, apiKey?: string) {
         const duration = Date.now() - start
         await db.query('INSERT INTO api_usage_logs (api_key, endpoint, method, status_code, response_time_ms, query_params) VALUES ($1, $2, $3, $4, $5, $6)', [apiKey || null, req.path, req.method, res.statusCode, duration, Object.keys(req.query)]);
 
-        console.log("Database log inserted successfully")
+        console.log("Database log inserted successfully");
     } catch (error) {
-        console.error(error)
-        throw error
+        console.error(error);
+        throw error;
     }
 }
 
@@ -44,12 +43,4 @@ export async function logger(req: Request, res: Response, apiKey?: string) {
 //     null,
 //     true
 // );
-
-// const aggregateLogs = cron.schedule('0 1 * * *', async () => {
-//     console.log("--CronJob Start--")
-//     await db.query(`INSERT INTO api_usage_aggregate (api_key, endpoint, date, request_count, avg_response_time_ms, error_count) SELECT api_key, endpoint, DATE_TRUNC('day', timestamp) AS date, COUNT(*) AS request_count, AVG(response_time_ms) AS avg_response_time_ms, COUNT(*) FILTER(WHERE status_code >= 400) AS error_count FROM api_usage_logs WHERE timestamp >= NOW() - INTERVAL '1 day' GROUP BY api_key, endpoint, DATE_TRUNC('day', timestamp)`)
-//     console.log("--CronJob End--")
-// })
-
-// aggregateLogs.start();
 
