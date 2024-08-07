96130b2dfef7db4bb9fc9d2d15c537e579c90023
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-08-07 22:44:35 +0100

    update vercel cronjob

--- src/utils/cron.ts
+++ src/utils/cron.ts
@@ -15,15 +15,15 @@ export default async function aggregateLogs(req: Request, res: Response) {
     try {
         const checkLogsToday = await db.query(`SELECT timestamp FROM api_usage_logs WHERE DATE(timestamp) = $1`, [yesterday]);
 
-        if (checkLogsToday.rows.length > 0) {
+        if (checkLogsToday.rows && checkLogsToday.rows.length > 0) {
             await db.query(`INSERT INTO api_usage_aggregate (api_key, endpoint, date, request_count, total_response_time_ms, error_count, query_params) SELECT api_key, endpoint, DATE_TRUNC('day', timestamp) AS date, COUNT(*) AS request_count, SUM(response_time_ms) AS total_response_time_ms, COUNT(*) FILTER(WHERE status_code >= 400) AS error_count, query_params FROM api_usage_logs WHERE timestamp >= NOW() - INTERVAL '1 day' GROUP BY api_key, endpoint, DATE_TRUNC('day', timestamp), query_params`);
             console.log(`Logs found on ${yesterday} & aggregated.`);
-        } else {
+        } else if (!checkLogsToday.rows) {
             await db.query('INSERT INTO api_usage_aggregate (api_key, endpoint, date, request_count, avg_response_time_ms, error_count, query_params) VALUES ($1, $2, $3, $4, $5, $6)', ['NULL', '/', yesterday, 0, 0, 0, []]);
             console.log(` No logs found on ${yesterday}. Inserted zeros.`);
         }
     } catch (error) {
-        console.error(error);
+        console.error("Error when handling logs.", error);
         throw error;
     }
 
