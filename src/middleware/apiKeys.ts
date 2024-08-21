83e57a3f9d9e6c4c06c3e140eca628040d2a3507
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-08-21 17:59:36 +0100

    Add bcrypt to validateApiKey middleware. Errors on Invalid & Missing keys

--- src/middleware/apiKeys.ts
+++ src/middleware/apiKeys.ts
@@ -1,34 +1,44 @@
 import express, { NextFunction, Request, Response } from "express";
 import { db } from "../lib/db"
 import { logger } from "./logger"
+import bcrypt from 'bcrypt';
+
 
 // Function to validate API key //
 export async function validateApiKey(req: Request, res: Response, next: NextFunction) { // exported to be used in router.ts
 
-    const apiKey = req.headers['x-api-key']; // define apiKey from headers of the request
+  const apiKey = req.headers['x-api-key'] as string; // define apiKey from headers of the request
 
-    if (!apiKey) {
-        res.status(401).json({ error: 'Unauthorized. Api Key is missing.' })
-        console.log('Unathorized. Api Key is missing.')
-        logger(req, res, 'MISSING_KEY');
-        return;
+  if (!apiKey) {
+    res.status(401).json({ error: 'Unauthorized. Api Key is missing.' })
+    console.log('Unathorized. Api Key is missing.')
+    logger(req, res, 0, 'MISSING_KEY');
+    return;
 
-    } else if (apiKey) {
-        try {
-            const findKey = await db.query('SELECT EXISTS(SELECT 1 FROM users WHERE api_key = $1)', [apiKey])
-            const keyExists = findKey.rows[0].exists
-            if (!keyExists) {
-                res.status(401).json({ error: 'Unauthorized. Api key is incorrect.' })
-                console.log('Unauthorized. Api key is incorrect.')
-                logger(req, res, `INVALID_KEY - ${apiKey}`)
-                return;
-            } else {
-                next() // if key matches, proceed to endpoint called.
-            }
-        } catch (error: any) {
-            console.error("Database query error: ", error)
+  } else if (apiKey) {
+
+    try {
+      const hashedKeys = await db.query('SELECT api_key FROM users');
+      let isAuthenticated: boolean = false;
+
+      for (const hashedKey of hashedKeys.rows) {
+        if (await bcrypt.compare(apiKey, hashedKey.api_key)) {
+          isAuthenticated = true;
+          break;
         }
+      }
+
+      if (!isAuthenticated) {
+        res.status(401).json({ error: 'Unauthorized. Api key is incorrect.' })
+        logger(req, res, 0, `INVALID_KEY - ${apiKey}`)
+      }
+
+      next();
+
+    } catch (error) {
+      console.error("Database Query Error: ", error);
     }
+  }
 }
 
 
