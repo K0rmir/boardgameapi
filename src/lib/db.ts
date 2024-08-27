f7ab7a5fbd3a28d2f808d883dec3c0bb1cbc7cfe
k0rmir <eddstewart1@hotmail.co.uk>
2024-08-27 11:23:25 +0100

    bugfix: cannot set headers after response is sent to client when api key is incorrect

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -7,12 +7,13 @@ dotenv.config();
 const dbConnectionString = process.env.supabaseUrl;
 
 if (!dbConnectionString) {
-    throw new Error("DATABASE_URL is not set in the environment variables"); // Throw error if no connection string is provided for database.
+  throw new Error("supabaseUrl is not set in the environment variables"); // Throw error if no connection string is provided for database.
 }
 
 export const db = new pg.Pool({ connectionString: dbConnectionString });
 
-db.on('error', (err) => { // add event listener 'on' to handle any unexpected errors that occur while client is idle.
-    console.error('Unexpected error on idle client', err);
-    process.exit(-1);
+db.on("error", (err) => {
+  // add event listener 'on' to handle any unexpected errors that occur while client is idle.
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
 });
--- src/middleware/apiKeys.ts
+++ src/middleware/apiKeys.ts
@@ -1,23 +1,27 @@
 import express, { NextFunction, Request, Response } from "express";
-import { db } from "../lib/db"
-import { logger } from "./logger"
-import bcrypt from 'bcrypt';
-
+import { db } from "../lib/db";
+import { logger } from "./logger";
+import bcrypt from "bcrypt";
 
 // Function to validate API key //
-export async function validateApiKey(req: Request, res: Response, next: NextFunction) { // exported to be used in router.ts
+export async function validateApiKey(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  // exported to be used in router.ts
 
-  const apiKey = req.headers['x-api-key'] as string; // define apiKey from headers of the request
-  let hashedApiKey: string = '';
+  const apiKey = req.headers["x-api-key"] as string; // define apiKey from headers of the request
+  let hashedApiKey: string = "";
 
   if (!apiKey) {
-    res.status(401).json({ error: 'Unauthorized. Api Key is missing.' });
-    console.log('Unauthorized. Api Key is missing.');
-    logger(req, res, null, 'MISSING_KEY');
+    res.status(401).json({ error: "Unauthorized. Api Key is missing." });
+    console.log("Unauthorized. Api Key is missing.");
+    logger(req, res, null, "MISSING_KEY");
     return;
   } else if (apiKey) {
     try {
-      const hashedKeys = await db.query('SELECT api_key FROM users');
+      const hashedKeys = await db.query("SELECT api_key FROM users");
       let isAuthenticated: boolean = false;
 
       for (const hashedKey of hashedKeys.rows) {
@@ -29,16 +33,14 @@ export async function validateApiKey(req: Request, res: Response, next: NextFunc
       }
 
       if (!isAuthenticated) {
-        res.status(401).json({ error: 'Unauthorized. Api key is incorrect.' });
+        res.status(401).json({ error: "Unauthorized. Api key is incorrect." });
         logger(req, res, null, `INVALID_KEY - ${hashedApiKey}`);
+        return;
       }
 
       next();
-
     } catch (error) {
       console.error("Database Query Error: ", error);
     }
   }
 }
-
-
